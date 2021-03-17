2c0a322a1898df702d0ebeed92dc001d51bf180c
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-03-17 13:15:12 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -1,6 +1,18 @@
 # My first code in R for remote sensing
 
-library(raster)
-setwd("C:/lab/") #path to folder  
-p224r63_2011 <- brick("p224r63_2011_masked.grd") #first image loaded
-plot(p224r63_2011) #see the image
+#install.packages("raster") to install the package "raster" 
+
+#once the package is installed, use it through the function library
+library(raster) 
+
+setwd("C:/lab/") #path to folder (where there are the files we want to use)  
+
+p224r63_2011 <- brick("p224r63_2011_masked.grd") #first image loaded through the function brick
+plot(p224r63_2011) #see the image loaded before
+#B1 = blue
+#B2 = green
+#B3 = red
+#B4 = near IR 
+#B5 = middle IR
+#B6 = thermal IR
+
