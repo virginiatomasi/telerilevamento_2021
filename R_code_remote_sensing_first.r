a71e49737e205a00462d1acccbc85733417afd59
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-03-17 15:31:23 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -5,7 +5,7 @@
 #once the package is installed, use it through the function library
 library(raster) 
 
-setwd("C:/lab/") #path to folder (where there are the files we want to use)  
+setwd("C:/lab/") #path to folder for Windows (where there are the files we want to use)  
 
 p224r63_2011 <- brick("p224r63_2011_masked.grd") #first image loaded through the function brick
 plot(p224r63_2011) #see the image loaded before
@@ -16,3 +16,10 @@ plot(p224r63_2011) #see the image loaded before
 #B5 = middle IR
 #B6 = thermal IR
 
+#colour change= function colorRampPalette
+cl <- colorRampPalette(c("black","grey","light grey"))(100)  
+#the letter c is used to define an array made of the same elements
+#the number 100 = number of levels between the colours
+
+#plot the image with new colours: plot(image, col=cl) -> function "col" is used to define the colours
+plot(p224r63_2011, col=cl)
