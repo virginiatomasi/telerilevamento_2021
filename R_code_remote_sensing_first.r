6b2500ee17a27557437afa266cf7b0f268b09892
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-03-24 12:47:12 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -19,7 +19,45 @@ plot(p224r63_2011) #see the image loaded before
 #colour change= function colorRampPalette
 cl <- colorRampPalette(c("black","grey","light grey"))(100)  
 #the letter c is used to define an array made of the same elements
-#the number 100 = number of levels between the colours (define the shades)
+#the number 100 = levels between the colours (define the shades)
 
 #plot the image with new colours: plot(image, col=cl) -> function "col" 
 plot(p224r63_2011, col=cl)
+
+#plot just one band
+#dev.off will clean the current graph
+dev.off()
+plot(p224r63_2011$B1_sre) #plot band B1 of the image
+
+#plot two bands at the same time:
+#function par, mf= multiframe, defining rows and columns
+par(mfrow=c(2,1)) 
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+
+#plot the first four bands of Landsat
+par(mfrow=c(4,1)) #4 rows and 1 column
+plot(p224r63_2011$B1_sre)
+plot(p224r63_2011$B2_sre)
+plot(p224r63_2011$B3_sre)
+plot(p224r63_2011$B4_sre)
+
+par(mfrow=c(2,2)) #2 rows and 2 columns
+plot(p224r63_2011$B1_sre, col=cl)
+plot(p224r63_2011$B2_sre, col=cl)
+plot(p224r63_2011$B3_sre, col=cl)
+plot(p224r63_2011$B4_sre, col=cl)
+
+#plot with different colours for every images
+par(mfrow=c(2,2))
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(p224r63_2011$B1_sre, col=clb)
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(p224r63_2011$B2_sre, col=clg)
+clr <- colorRampPalette(c("dark red","red","pink"))(100)
+plot(p224r63_2011$B3_sre, col=clr)
+clnir <- colorRampPalette(c("red","orange","yellow"))(100)
+plot(p224r63_2011$B4_sre, col=clnir)
+
+
+
