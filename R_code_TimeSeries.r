22ab920961a372a195964301cbd36b3c4fdc92db
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-03-31 13:00:18 +0200

    Update R_code_TimeSeries.r

--- R_code_TimeSeries.r
+++ R_code_TimeSeries.r
@@ -1,1 +1,7 @@
+#time series analysis
+#Greenland increase of temperature
+#data and code from Emanuela Cosma
 
+#package raster and set working directory
+library(raster)
+setwd("C:/lab/greenland")
