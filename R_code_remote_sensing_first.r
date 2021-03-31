acd10aa019e25b287ec9dcf9540ada4863b3d6b7
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-03-31 12:41:51 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -7,7 +7,8 @@ library(raster)
 
 setwd("C:/lab/") #path to folder for Windows (where there are the files we want to use)  
 
-p224r63_2011 <- brick("p224r63_2011_masked.grd") #first image loaded through the function "brick"
+#first image loaded through the function "brick" 
+p224r63_2011 <- brick("p224r63_2011_masked.grd") 
 plot(p224r63_2011) #see the image loaded before
 #B1 = blue
 #B2 = green
@@ -25,7 +26,7 @@ cl <- colorRampPalette(c("black","grey","light grey"))(100)
 plot(p224r63_2011, col=cl)
 
 #plot just one band
-#dev.off will clean the current graph
+#dev.off will clean the current graph (closing the image)
 dev.off()
 plot(p224r63_2011$B1_sre) #plot band B1 of the image
 
@@ -59,9 +60,9 @@ plot(p224r63_2011$B3_sre, col=clr)
 clnir <- colorRampPalette(c("red","orange","yellow"))(100)
 plot(p224r63_2011$B4_sre, col=clnir)
 
-#natural colours: "plotRGB" to plot an image using RGB bands, stretching the bands (Lin= linear function)
-plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin")
-plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") #no blue band (what was in the red band, now is in nir band)
+#"plotRGB" to plot an image using RGB bands, stretching the bands (Lin= linear function)
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="Lin") #natural colours
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin") #no blue band, red=vegetation (what was in the red band, now is in nir band)
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin") #purple in the image is bare soil
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin") #yellow in the image is bare soil
 
@@ -75,7 +76,7 @@ plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 dev.off()
 
 #"plotRGB" to plot an image using RGB bands, stretching the bands (histogram stratching function= hist)
-plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist") #hist stretching is useful to see much more details
 
 #par natural colours, false colours and false colours with histogram stratching
 par(mfrow=c(3,1)) 
@@ -85,11 +86,31 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
 
 
+#MULTITEMPORAL SET
+library(raster) 
+setwd("C:/lab/")
+p224r63_2011 <- brick("p224r63_2011_masked.grd") #import the old image
+p224r63_1988 <- brick("p224r63_1988_masked.grd") #import the new image
 
+plot(p224r63_1988) #plot all the bands
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin") #plot RGB (natural colours)
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin") #red band -> nir band (what is red= vegetation)
 
-
-
-
-
-
-
+#see the differences between 1988 and 2011
+par(mfrow=c(2,1)) 
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+# differences between 1988 and 2011, with different stretching
+par(mfrow=c(2,2))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+#save the image above as a .pdf
+pdf("1988_vs_2011.pdf")
+par(mfrow=c(2,2))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="hist")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+dev.off()
