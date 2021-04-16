f640e85fb91ef7faaa3759c6b5bb9cc650c45a34
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-04-16 10:04:50 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -5,5 +5,6 @@ setwd("C:/lab/") #set working directory for Windows
 
 library(knitr)
 
-
+#stitch function
+stitch("R_code_greenland.r.docx", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
