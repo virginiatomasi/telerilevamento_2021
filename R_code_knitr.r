e8b8d0b76221b89de44a5f2fffd32892c27ed661
Virginia Tomasi <80533943+virginiatomasi@users.noreply.github.com>
2021-04-16 10:46:21 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -6,5 +6,10 @@ setwd("C:/lab/") #set working directory for Windows
 library(knitr)
 
 #stitch function
-stitch("R_code_greenland.r.docx", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("R_code_Greenland.r.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
+
+#if problems appear:
+#install.packages("tinytex")
+#tinytex::install_tinytex()
+#tinytex::tlmgr_update()
